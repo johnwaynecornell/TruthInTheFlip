212f6a3ec69d240d5e3d506dbbcb2670e59060a1
jwc <john.wayne.cornell@gmail.com>
2026-07-12 11:55:12 -0400

    Add TruthInTheFlio_sample_test_quantis project: implement sample test for QuantisInterop entropy and white random buffer generation

---
+++ TruthInTheFlio_sample_test_quantis/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using TruthInTheFlip.Format;
+
+Random r = new Random();
+Func<Action<byte[]>>? resetRandom = QuantisInterop.QuantisFactory(true, 0, QuantisInterop.DeviceType.PCI);
+Func<Action<byte[]>>? resetRandom2 = QuantisInterop.QuantisFactory(false, 0, QuantisInterop.DeviceType.PCI);
+
+var fill = resetRandom();
+byte[] buffer = new byte[1024];
+fill(buffer);
+Dump("\nentropy random:", buffer);
+
+resetRandom2()(buffer);
+Dump("\nwhite random:", buffer);
+
+fill(buffer);
+Dump("\nentropy random:", buffer);
+
+
+void Dump(string message, byte[] buffer)
+{
+    StringBuilder sb = new StringBuilder();
+    sb.AppendLine(message);
+    
+    int c = 0;
+    for (int position = 0; position < buffer.Length; position += 8)
+    {
+        sb.Append("[ ");
+        sb.Append(BitConverter.ToString(buffer, position, 8));
+        sb.Append(" ]");
+
+        c++;
+        if (c == 16)
+        {
+            sb.AppendLine();
+            c = 0;
+        }
+        
+        Console.WriteLine(sb.ToString());
+    }
+}
+    
